29f6654ed98f60f92e686b00953ff8cb72f085af
Hyeonwoo Daniel Yoo <hyoen95y@gmail.com>
2018-11-11 19:05:08 +0900

    Refreshed local history

--- preprocess_patches.py
+++ preprocess_patches.py
@@ -19,24 +19,26 @@ patchlist = [
 
 # RESIZE IMAGES
 import cv2
-width = 35
-height = 35
 
-def resizing_images(input) :
+def resizing_images(input, width, height) :
     x = []
     for img in input:
-        base = os.path.basename(img)
-        # Read and resize image
         full_size_image = cv2.imread(img)
         x.append(cv2.resize(full_size_image, (width,height), interpolation=cv2.INTER_CUBIC))
     return x
 
-hair_train = resizing_images(patchlist[0])
-hair_test = resizing_images(patchlist[1])
-nonhair_train = resizing_images(patchlist[2])
-nonhair_test = resizing_images(patchlist[3])
+def creating_npz_file(size) :
+    size = size
+    width = size
+    height = size
+    hair_train = resizing_images(patchlist[0], width, height)
+    hair_test = resizing_images(patchlist[1], width, height)
+    nonhair_train = resizing_images(patchlist[2], width, height)
+    nonhair_test = resizing_images(patchlist[3], width, height)
+    np.savez(os.path.normpath(os.path.join(__file__,'../', 'Patch1k_resized_'+str(size)+'.npz')), hair_train=hair_train, hair_test=hair_test, nonhair_train=nonhair_train, nonhair_test=nonhair_test)
+    print('* Done at size : ', size)
 
-np.savez(os.path.normpath(os.path.join(__file__,'../', 'Patch1k_resized_35.npz')), hair_train=hair_train, hair_test=hair_test, nonhair_train=nonhair_train, nonhair_test=nonhair_test)
-
-with np.load(os.path.normpath(os.path.join(__file__,'../', 'Patch1k_resized.npz'))) as fh :
+'''
+with np.load(os.path.normpath(os.path.join(__file__,'../', 'Patch1k_resized_'+str(size)+'.npz'))) as fh :
     print(fh.files)
+'''
